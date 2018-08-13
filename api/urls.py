53e528d23cfcf6854db35e2cbe013f119fe87062
raaul- <raaul-@live.com.mx>
2018-08-13 17:09:24 -0500

    Add create short url method

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create, name='create')
+    path('', views.createShortURL, name='createShortURL')
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
+
+import json
+
 from django.http import JsonResponse
 from django.http import HttpResponseBadRequest
 
-def create(request):
+from api.models import ShortLink
+
+from os import urandom
+from base64 import b32encode
+
+def createShortURL(request):
     if request.method == 'POST':
         if not request.body:
             return HttpResponseBadRequest("Bad request")
         
+        jsonRequest = None
+        urllist = []
+        try:
+            jsonRequest = json.loads(request.body)
+        except json.decoder.JSONDecodeError:
+            return HttpResponseBadRequest("Bad request")
+        
+        if isinstance(jsonRequest, list):
+            urllist = jsonRequest
+        else:
+            urllist.append(jsonRequest['url'])
+        
+        shortList = []
+        for url in urllist:
+            shortKey = b32encode(urandom(5)).decode('ascii')
+            shortUrl =  'http://' + request.get_host() + '/' + shortKey
+            shortLink = ShortLink(original=url, shortKey=shortKey)
+            shortLink.save()
+            shortList.append({
+                'original': url,
+                'short': shortUrl
+            })
+        return JsonResponse(shortList, safe=False)
     else:
         return HttpResponseBadRequest("Bad request")
